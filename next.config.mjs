88883bddbed72082a0ba8c31ec09f8e25d1f6195
FrhCode <farhan7534031b@gmail.com>
2024-05-31 10:41:14 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "cdn.pixabay.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
+        hostname: "mandiri-api.farhandev.cloud",
         port: "",
         pathname: "/**",
       },
